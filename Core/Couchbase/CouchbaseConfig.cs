2306a08237b753aa97984821a23fb83408928b74
Quan Trong Anh Huy <huyqta@live.com>
2015-09-09 02:06:03 +0700

    Add core project

---
+++ Core/Couchbase/CouchbaseConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Couchbase;
+using System.Configuration;
+
+namespace Core.Couchbase
+{
+    public static class CouchbaseConfig
+    {
+        public static Cluster CbCluster;
+
+        public static string CouchbaseBucket = ConfigurationManager.AppSettings["CouchbaseBucket"];
+
+        public static string CouchbaseUser = ConfigurationManager.AppSettings["CouchbaseUser"];
+
+        public static string CouchbasePassword = ConfigurationManager.AppSettings["CouchbasePassword"];
+
+        public static string CouchbaseServer = ConfigurationManager.AppSettings["CouchbaseServer"];
+    }
+}
---
+++ Core/Repositories/CbRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Couchbase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repositories
+{
+    public class CbRepository
+    {
+        public void InitCouchbase()
+        {
+            if (CouchbaseConfig.CbCluster == null)
+            {
+                //var listUri = CouchbaseConfig.CouchbaseServer.Split('|').ToList();
+                //CouchbaseConfig.CbCluster.Configuration.Servers = listUri.ToList<Uri>();
+
+            }
+        }
+    }
+}
